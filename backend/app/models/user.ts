f20eae2c3c881cc12591d117f356ff87cf2bf7de
ArnaudDoubremelle <arnaud.doub@gmail.com>
2024-07-31 11:38:45 +0200

    init: setup backend

---
+++ backend/app/models/user.ts
@@ -0,0 +1,41 @@
+import { DateTime } from 'luxon'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import hash from '@adonisjs/core/services/hash'
+import crypto from 'node:crypto'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  static readonly selfAssignPrimaryKey = true
+
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare fullName: string
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  @beforeCreate()
+  static assignUuid(user: User) {
+    user.id = crypto.randomUUID()
+  }
+
+  static readonly accessTokens = DbAccessTokensProvider.forModel(User)
+}
